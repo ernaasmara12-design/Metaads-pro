1acfbf8739cc455ed62a716b2adabbce52a77898
ernaasmara12-design <erna.asmara12@gmail.com>
2026-07-25 05:11:31 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="MetaAds Pro",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("🚀 MetaAds Pro")
+st.write("Selamat datang di MetaAds Pro")
